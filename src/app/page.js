87590137adc287a09416f2a1fb600c8b1cd3635e
Tazim <tazim0877@gmail.com>
2025-03-21 00:41:56 +0600

    feat : created Navbar component

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,20 @@
+import Navbar from "../components/Navbar.js";
+
 export default function Home() {
-  return <div className=" text-6xl">Welcome to NaiveOJ!</div>;
+  return (
+    <div className=" max-w-11/12 m-auto">
+      {/* Navbar Component */}
+      <Navbar />
+
+      {/* Main Content */}
+      <main className="p-8">
+        <h1 className="text-4xl font-bold mb-4">Welcome to NaiveOJ</h1>
+        <p className="text-lg">
+          NaiveOJ is your go-to platform for competitive programming and coding
+          challenges. Join us to sharpen your skills, compete in contests, and
+          solve interesting problems.
+        </p>
+      </main>
+    </div>
+  );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,56 @@
 import React from "react";
+import Link from "next/link";
 
 function Navbar() {
-  return <div>Navbar</div>;
+  const isAuthenticated = false;
+
+  return (
+    <nav className="flex bg-background justify-between items-center p-4 ">
+      {/* Left side - Logo */}
+
+      <div className="text-2xl font-bold">
+        <Link href="/">NOJ</Link>
+      </div>
+
+      {/* Center - Navigation Links */}
+      <div className="space-x-4">
+        <Link
+          href="/problemset"
+          className=" font-medium hover:text-gray-300 transition duration-300"
+        >
+          Problemset
+        </Link>
+        <Link
+          href="/contests"
+          className="font-medium hover:text-gray-600 transition duration-300"
+        >
+          Contests
+        </Link>
+        <Link
+          href="/about"
+          className="font-medium hover:text-gray-600 transition duration-300"
+        >
+          About Us
+        </Link>
+      </div>
+
+      {/* Right side - Join Now/Profile button */}
+      <div>
+        {!isAuthenticated ? (
+          <Link
+            href="/join"
+            className="px-4 py-2 border border-secondary text-secondary rounded-lg font-bold transition duration-300  hover:outline-1 hover:outline-secondary"
+          >
+            Join Now
+          </Link>
+        ) : (
+          <button className="px-4 py-2 bg-red-600 text-white rounded-lg hover:bg-red-500">
+            Profile
+          </button>
+        )}
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
