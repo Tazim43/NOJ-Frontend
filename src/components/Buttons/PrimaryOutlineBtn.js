b4375845b79189a73bdc9175840a35299d5fda77
Tazim <tazim0877@gmail.com>
2025-03-24 00:37:07 +0600

    Update SecondaryOutlineBtn and PrimaryOutlineBtn styles, add global colors
    - Refined styles for SecondaryOutlineBtn and PrimaryOutlineBtn components.
    - Added global color variables for primary light & dark and secondary light & dark

--- src/components/Buttons/PrimaryOutlineBtn.js
+++ src/components/Buttons/PrimaryOutlineBtn.js
@@ -1,14 +1,18 @@
 import React from "react";
 import Link from "next/link";
 
-function PrimaryOutlineBtn({ url = "#", text = "Button" }) {
-  return (
-    <Link
-      href={url}
-      className="px-4 py-1 border border-primary text-primary rounded-lg font-medium transition duration-300  hover:outline-1 hover:outline-primary"
-    >
+function PrimaryOutlineBtn({ url, text = "Button", onClick }) {
+  const commonClasses =
+    "px-4 py-2 border border-primary cursor-pointer text-primary rounded-lg font-bold transition duration-300 hover:outline-1 hover:outline-primary";
+
+  return url ? (
+    <Link href={url} className={commonClasses}>
       {text}
     </Link>
+  ) : (
+    <button onClick={onClick} className={commonClasses}>
+      {text}
+    </button>
   );
 }
 
--- src/components/Buttons/SecondaryOutlineBtn.js
+++ src/components/Buttons/SecondaryOutlineBtn.js
@@ -1,14 +1,18 @@
 import React from "react";
 import Link from "next/link";
 
-function SecondaryOutlineBtn({ url = "#", text = "Button" }) {
-  return (
-    <Link
-      href={url}
-      className="px-4 py-2 border border-secondary text-secondary rounded-lg font-bold transition duration-300  hover:outline-1 hover:outline-secondary"
-    >
+function SecondaryOutlineBtn({ url, text = "Button", onClick }) {
+  const commonClasses =
+    "px-4 py-2 border border-secondary cursor-pointer text-secondary rounded-lg font-bold transition duration-300 hover:outline-1 hover:outline-secondary";
+
+  return url ? (
+    <Link href={url} className={commonClasses}>
       {text}
     </Link>
+  ) : (
+    <button onClick={onClick} className={commonClasses}>
+      {text}
+    </button>
   );
 }
 
