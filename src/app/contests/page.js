b034f26dacabcbc364ae5abf69b69d4b890b813f
Tazim <tazim0877@gmail.com>
2025-07-11 14:43:41 +0600

    feat : upcoming contest page

---
+++ src/app/contests/page.js
@@ -0,0 +1,13 @@
+"use client";
+
+export default function ContestPage() {
+  return (
+    <div className="p-8 min-h-screen text-white">
+      <h2 className="text-3xl font-semibold mb-6">All Contests</h2>
+
+      <div className="bg-gray-900 rounded-lg p-6 flex items-center justify-between text-gray-400">
+        <p className="text-lg">🚫 No upcoming contests at the moment.</p>
+      </div>
+    </div>
+  );
+}
